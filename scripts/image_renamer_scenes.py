f59edd9e76123f43ee77117cccd0dd8558c9b417
Mitchell Pomery <bob_george33@hotmail.com>
2015-05-24 13:28:14 +0800

    Missed a '+'

--- scripts/image_renamer_scenes.py
+++ scripts/image_renamer_scenes.py
@@ -5,7 +5,7 @@ files = [ f for f in os.listdir('.') if os.path.isfile(f) and f.endswith('.jpg')
 image_number = 1
 
 for file in files:
-    newname = 'scene_'str(image_number) + '.jpg'
+    newname = 'scene_' + str(image_number) + '.jpg'
     print('renaming ' + str(file) + ' to ' + str(newname))
     os.rename(file, newname)
     image_number += 1
